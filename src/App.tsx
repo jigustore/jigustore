c0f124fc728c87498bc5e3bc7b5921aca7d4906e
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2026-04-03 15:19:28 +0545

    add: basic init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className="w-screen h-screen justify-center items-center text-orange-400">
+      Welcome to Jigu Store
+    </main>
+  )
+}
+
+export default App
